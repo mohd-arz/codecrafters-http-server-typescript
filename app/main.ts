06aa3069f120ee3b6898718a0f356dd19b05c2ba
mohd-arz <mohammedarsh75@gmail.com>
2024-07-08 10:00:04 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -6,7 +6,15 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data",(data)=>{
-    socket.write("HTTP/1.1 200 OK\r\n\r\n")
+    const request = data.toString();
+    const [requestLine] = request.split('\r\n');
+    const [method,url,version] =requestLine.split(' ');
+    if(url == '/'){
+      socket.write("HTTP/1.1 200 OK\r\n\r\n")
+    }else{
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+    }
+    socket.end();
   })
   socket.on("close", () => {
     socket.end();
